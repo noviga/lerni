9c5c316cc4f3ad69e3d1e7bc7adb44865f488a69
S. Yakupov <syakupov@users.noreply.github.com>
2022-11-05 17:03:38 +0300

    Improve navigation in `Cards` (#3)

--- src/text.rs
+++ src/text.rs
@@ -10,9 +10,9 @@ pub struct Props {
 #[function_component(Text)]
 pub fn text(props: &Props) -> Html {
     html! {
-    <svg viewBox="0 0 640 360" class="has-ratio">
-        <rect width="100%" height="100%" rx="10" fill={ props.background.clone() } />
-        <text x="50%" y="50%" font-size="48" text-anchor="middle" dominant-baseline="middle">{ props.text.clone() }</text>
-    </svg> 
+        <svg viewBox="0 0 640 360" class="has-ratio">
+            <rect width="100%" height="100%" rx="10" fill={ props.background.clone() } />
+            <text x="50%" y="50%" font-size="48" text-anchor="middle" dominant-baseline="middle">{ props.text.clone() }</text>
+        </svg>
     }
 }
