ece60b24e42eccc65c37e075344a145768fdfbd1
Shamil <shamilsan@users.noreply.github.com>
2022-11-06 21:57:26 +0300

    Rename attributes to be more like usual HTML ones (#6)

--- src/layout/grid.rs
+++ src/layout/grid.rs
@@ -8,12 +8,13 @@ pub struct Props {
     #[prop_or(1)]
     pub rows: usize,
     #[prop_or(1)]
-    pub columns: usize,
+    pub cols: usize,
 }
 
+/// Grid layout element.
 #[function_component(Grid)]
 pub fn grid(props: &Props) -> Html {
-    let n = props.columns.max(props.rows);
+    let n = props.cols.max(props.rows);
 
     let sx = 1.0 / n as f64;
     let sy = 1.0 / n as f64;
@@ -21,13 +22,13 @@ pub fn grid(props: &Props) -> Html {
     let w = SVG_WIDTH / n;
     let h = SVG_HEIGHT / n;
 
-    let dw = (SVG_WIDTH - w * props.columns) / (props.columns + 1);
+    let dw = (SVG_WIDTH - w * props.cols) / (props.cols + 1);
     let dh = (SVG_HEIGHT - h * props.rows) / (props.rows + 1);
 
     html! {
-        for props.children.iter().take(props.columns * props.rows).enumerate().map(|(i, child)| {
-            let tx = dw + (i % props.columns) * (w + dw);
-            let ty = dh + (i / props.columns) * (h + dh);
+        for props.children.iter().take(props.cols * props.rows).enumerate().map(|(i, child)| {
+            let tx = dw + (i % props.cols) * (w + dw);
+            let ty = dh + (i / props.cols) * (h + dh);
             let transform = format!("translate({tx} {ty}) scale({sx} {sy})");
             html_nested!(<g { transform }>{ child }</g>)
         })
