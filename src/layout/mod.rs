dee86f827bddbda0de928672d2dcc958f8b6e33a
S. Yakupov <syakupov@users.noreply.github.com>
2022-11-06 11:37:00 +0300

    Remove `Frame`, use `Slide` instead (#5)

--- src/layout/mod.rs
+++ src/layout/mod.rs
@@ -1,10 +1,8 @@
-mod frame;
 mod grid;
 mod properties;
 mod slide;
 mod slideshow;
 
-pub use frame::Frame;
 pub use grid::Grid;
 pub use properties::{Align, VAlign, SVG_HEIGHT, SVG_WIDTH};
 pub use slide::Slide;
