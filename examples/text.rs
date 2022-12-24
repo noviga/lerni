5cca86c5b99f6356ee2bcf5fba6ed596be5e4cac
Shamil <shamilsan@users.noreply.github.com>
2022-12-24 22:25:12 +0300

    Improve text widget (#25)

--- examples/text.rs
+++ examples/text.rs
@@ -4,17 +4,50 @@ use yew::prelude::*;
 
 #[function_component]
 pub fn TextExample() -> Html {
+    let x = use_state(|| 0);
+    let y = use_state(|| 0);
+    let onclick = {
+        let x = x.clone();
+        let y = y.clone();
+        Callback::from(move |(mx, my)| {
+            x.set(mx);
+            y.set(my);
+        })
+    };
+
+    let read1 = use_state(|| "".to_string());
+    let onread1 = {
+        let read1 = read1.clone();
+        Callback::from(move |(letters, total)| {
+            read1.set(format!("{letters} / {total}"));
+        })
+    };
+
+    let read2 = use_state(|| "".to_string());
+    let onread2 = {
+        let read2 = read2.clone();
+        Callback::from(move |(letters, total)| {
+            read2.set(format!("{letters} / {total}"));
+        })
+    };
+
     html! {
-        <Slide>
-            <Row padding=20 border_width=4>
-                <Text>
-                    { "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." }
-                    { "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }
-                </Text>
-                <Text font_size=72 bold=true font="serif">
-                    { "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." }
-                    { "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." }
-                </Text>
+        <Slide { onclick }>
+            <Row padding=30 border_width=4>
+                <Column stretch={ vec![5, 1] }>
+                    <Text marker_x={ *x } marker_y={ *y } onread={ onread1 }>
+                        { "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." }
+                        { "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }
+                    </Text>
+                    <Label text={ (*read1).clone() } />
+                </Column>
+                <Column stretch={ vec![5, 1] }>
+                    <Text font_size=72 bold=true font="serif" marker_x={ *x } marker_y={ *y } onread={ onread2 }>
+                        { "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." }
+                        { "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." }
+                    </Text>
+                    <Label text={ (*read2).clone() } />
+                </Column>
             </Row>
         </Slide>
     }
