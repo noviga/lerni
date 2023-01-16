cf0c86f892d93f00036fa055660695dd3c147a82
S. Yakupov <syakupov@users.noreply.github.com>
2023-01-16 11:20:17 +0300

    Fix label alignment

--- examples/grid.rs
+++ examples/grid.rs
@@ -15,18 +15,18 @@ pub fn GridExample() -> Html {
                 <Label html={ html!(<><tspan>{ "00" }</tspan><tspan fill="red">{ "3" }</tspan></>) } />
                 <Label text="4" font_size=96 color={ Color::Blue } />
                 <Button text="5" align={ Align::Fill } valign={ VAlign::Fill } />
-                <Label text="6" />
-                <Label text="7" />
+                <Label text="6" align={ Align::Right } />
+                <Label text="7" align={ Align::Left } />
                 <Label text="8" />
                 <Grid cols=4 rows=2 border_width=4 spacing=20>
-                    <Label text="9"  />
-                    <Label text="10" />
+                    <Label text="9" valign={ VAlign::Top } />
+                    <Label text="10" valign={ VAlign::Bottom } />
                     <Label text="11" />
                     <Button text="12" align={ Align::Fill } valign={ VAlign::Fill } />
                     <Label text="13" />
-                    <Label text="14" />
+                    <Label text="14" align={ Align::Left } valign={ VAlign::Top } />
                     <Label text="15" />
-                    <Label text="16" />
+                    <Label text="16" align={ Align::Right } valign={ VAlign::Bottom } />
                 </Grid>
             </Grid>
         </Slide>
