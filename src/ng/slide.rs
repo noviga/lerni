ca0e4ee9b884111958ff76085a3a58f323cbd55d
S. Yakupov <syakupov@users.noreply.github.com>
2023-07-22 19:40:47 +0300

    chore: clean up

--- src/ng/slide.rs
+++ src/ng/slide.rs
@@ -13,7 +13,7 @@ pub fn Slide(
     #[prop(optional)] background_color: Color,
     #[prop(optional)] _background_image: String,
     #[prop(optional)] _pointer: bool,
-    #[prop(optional)] blur: bool,
+    #[prop(optional)] _blur: bool,
     #[prop(default = 15)] _blur_radius: i32,
     children: Children,
 ) -> impl IntoView {
@@ -24,24 +24,15 @@ pub fn Slide(
     };
     provide_context(cx, frame);
 
-    let view_box = format!("0 0 {} {}", width, height);
-
-    let children = children(cx)
-        .nodes
-        .into_iter()
-        .map(|child| {
-            view! { cx, <>{child}</> }
-        })
-        .collect_view(cx);
+    let view_box = format!("0 0 {width} {height}");
 
     view! { cx,
         <div style="max-width: 100%;" class="container pl-4 mt-4 pr-4">
             <div class="box">
-                <figure class="image is-16by9" style={ blur }>
-                    <svg viewBox={ view_box } class="has-ratio"
-                        { onmousemove } { onmouseleave } { onclick }>
-                        <rect width="100%" height="100%" rx="10" ry="10" fill={ background_color.to_string() } />
-                        {children}
+                <figure class="image is-16by9">
+                    <svg viewBox=view_box class="has-ratio">
+                        <rect width="100%" height="100%" rx="10" ry="10" fill=background_color/>
+                        {children(cx)}
                     </svg>
                 </figure>
             </div>
