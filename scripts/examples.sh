176f10473c43ae679e0fc8e41087f7ea45ec7443
S. Yakupov <syakupov@users.noreply.github.com>
2023-07-30 15:29:42 +0300

    fix: script

--- scripts/examples.sh
+++ scripts/examples.sh
@@ -4,7 +4,7 @@ export PROFILE=$1
 if [ -z "$PROFILE" ]; then
     export PROFILE="debug"
 fi
-if [ "$PROFILE" == "release" ]; then
+if [ "$PROFILE" = "release" ]; then
     export FLAG="--release"
 fi
 
@@ -32,7 +32,7 @@ do
     WASM_IN="target/wasm32-unknown-unknown/$PROFILE/examples/$EXAMPLE.wasm"
     wasm-bindgen $WASM_IN --target web --out-dir "dist/$EXAMPLE" --no-typescript
     WASM_OUT="dist/$EXAMPLE/${EXAMPLE}_bg.wasm"
-    if [ "$PROFILE" == "release" ]; then
+    if [ "$PROFILE" = "release" ]; then
         wasm-opt $WASM_IN -o $WASM_OUT -Oz -c --dae --dce --rse -s 4 --vacuum
     fi
 done
